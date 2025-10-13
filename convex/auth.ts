13c00289deab268016b7a3dd72564b9d10fce8c8
Rafael Caceres <rafael@taller.net.br>
2025-10-12 23:38:24 -0300

    feat: fix deployment erros.

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,12 +1,27 @@
 import { convexAuth, getAuthUserId } from "@convex-dev/auth/server";
 import { Password } from "@convex-dev/auth/providers/Password";
 import { query } from "./_generated/server";
+import { v } from "convex/values";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [Password],
 });
 
 export const loggedInUser = query({
+  args: {},
+  returns: v.union(
+    v.null(), 
+    v.object({
+      _id: v.id("users"),
+      _creationTime: v.number(),
+      email: v.optional(v.string()),
+      emailVerificationTime: v.optional(v.number()),
+      image: v.optional(v.string()),
+      name: v.optional(v.string()),
+      phone: v.optional(v.string()),
+      phoneVerificationTime: v.optional(v.number()),
+    })
+  ),
   handler: async (ctx) => {
     const userId = await getAuthUserId(ctx);
     if (!userId) {
